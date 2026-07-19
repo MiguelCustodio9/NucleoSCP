ff3744d3432aa4d6cb9bcaccbe1a16d79cd49e6c
Miguel Custódio <153029138+MiguelCustodio9@users.noreply.github.com>
2026-07-19 21:55:52 +0100

    Update Supabase URL in config.js

--- config.js
+++ config.js
@@ -3,5 +3,5 @@
 // Substitui os valores abaixo pelos do teu projeto:
 // Supabase > Project Settings > API
 // =========================================================
-const SUPABASE_URL = 'https://lwehgnistggtekqbshur.supabase.co';
+const SUPABASE_URL = 'https://jiqiksnjeoycrgzfctif.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImppcWlrc25qZW95Y3JnemZjdGlmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ0ODQ0MTQsImV4cCI6MjEwMDA2MDQxNH0.V6H49fDZY5bR2eBBX66IeAMJCYfOC57HH2JDHKSe0PQ';
